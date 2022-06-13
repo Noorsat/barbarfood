c51d46b81b38141fa00f5bc47984080893f3db77
Nursat Bakyt <itsnursat@gmail.com>
2022-06-13 22:25:57 +0600

    fix

--- src/http/index.js
+++ src/http/index.js
@@ -2,11 +2,6 @@ import axios from 'axios';
 
 const myAxios = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    headers:{
-        "Access-Control-Allow-Origin":"*",
-        "Access-Control-Allow-Methods":"GET, PUT, POST",
-        "Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept"
-    }
 })
 
 export { myAxios }
